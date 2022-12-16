b2980a6629d9692901e6060e8202835c034bd2d8
achille56 <leclercqjm59@gmail.com>
2022-12-16 15:39:52 +0100

    exo 108

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+// Définition d'un objet au format JSON
+let nameJson = {
+    "nom": "Upto",
+    "adresse": "rue fictive numéro 65 à Fourmies",
+    "utilisateur": {
+        "nom": "Doe",
+        "prenom": "John",
+        "passion": [
+            "danser",
+            "travailler",
+            "sortir en boîte"
+        ]
+    }
+}
+console.log(nameJson);
